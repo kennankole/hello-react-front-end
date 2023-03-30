65c99295deabcfe209574f51ed9989e87419185e
kennankole <kennankole@gmail.com>
2023-03-30 20:13:17 +0300

    Setting Greetings component as a route in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greetings from './features/greetings/Greetings';
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <Greetings />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Greetings />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 export default App;
